59a310a9e1bab897cd6c27d168e1a372694aaecf
joshin <xjcute@gmail.com>
2016-12-08 11:17:57 +0800

    try

---
+++ DTiseChart/DTChart/DTChartBaseComponent.h
@@ -0,0 +1,13 @@
+//
+//  DTChartBaseComponent.h
+//  DTiseChart
+//
+//  Created by Jo Shin on 2016/12/8.
+//  Copyright © 2016年 studio.joshin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTChartBaseComponent : UIView
+
+@end
---
+++ DTiseChart/DTChart/DTChartBaseComponent.m
@@ -0,0 +1,21 @@
+//
+//  DTChartBaseComponent.m
+//  DTiseChart
+//
+//  Created by Jo Shin on 2016/12/8.
+//  Copyright © 2016年 studio.joshin. All rights reserved.
+//
+
+#import "DTChartBaseComponent.h"
+
+@implementation DTChartBaseComponent
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
